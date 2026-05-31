10e54fe85cd684776c06e03c72199230a071abff
정지영 <jeongjiyeong486@gmail.com>
2026-05-31 12:17:14 +0900

    fix: 상세 진입 출처별 탭 선택 상태 유지 Refs #77
    - ItemGuideDetailRoute에 상세 진입 출처를 추가
    - Search에서 상세로 진입하면 Search 탭 선택 상태 유지
    - Favorites에서 상세로 진입하면 Favorites 탭 선택 상태 유지
    - ItemGuideDetailSource를 @Serializable enum class로 정의
    - bottom navigation 선택 판별을 공식 hasRoute<T>() 기준으로 정리

--- common/src/main/java/com/team/yeogibeoryeo/common/navigation/NavControllerExtensions.kt
+++ common/src/main/java/com/team/yeogibeoryeo/common/navigation/NavControllerExtensions.kt
@@ -1,11 +1,7 @@
 package com.team.yeogibeoryeo.common.navigation
 
-import androidx.navigation.NavDestination
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
-
-inline fun <reified T : Any> NavDestination?.hasRouteName(): Boolean =
-    this?.route?.substringBefore("?") == requireNotNull(T::class.qualifiedName)
 
 inline fun <reified T : Any> NavHostController.navigateBottomTab(route: T) {
     navigate(route) {
