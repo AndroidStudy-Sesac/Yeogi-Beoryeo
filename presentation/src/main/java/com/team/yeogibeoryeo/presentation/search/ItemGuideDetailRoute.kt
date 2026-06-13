c77a424bf64fecca75649336e156adf24f721cb5
정지영 <jeongjiyeong486@gmail.com>
2026-06-13 15:54:14 +0900

    refactor: 즐겨찾기 스낵바 위치 정리
    - FavoriteSnackbar를 presentation common components로 이동
    
    - 품목 상세의 FavoriteSnackbar import 경로 수정
    
    - presentation 컴파일로 이동 범위 확인
    
    Refs #131

--- presentation/src/main/java/com/team/yeogibeoryeo/presentation/search/ItemGuideDetailRoute.kt
+++ presentation/src/main/java/com/team/yeogibeoryeo/presentation/search/ItemGuideDetailRoute.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.team.yeogibeoryeo.presentation.R
-import com.team.yeogibeoryeo.presentation.favorites.components.FavoriteSnackbar
+import com.team.yeogibeoryeo.presentation.common.components.FavoriteSnackbar
 import com.team.yeogibeoryeo.presentation.search.components.ItemSearchStatusDescription
 import com.team.yeogibeoryeo.presentation.search.components.ItemSearchStatusContent
 import com.team.yeogibeoryeo.presentation.search.components.ItemSearchLoadingContent
